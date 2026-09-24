fb0a6c5ed5cd9718376ed18e5712442cac639ab4
rodomega <108682614+rodomega@users.noreply.github.com>
2026-09-24 02:46:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => new Response('Offline')));
+// sw.js - Service Worker Básico
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    // Interceptação de requisições se necessário futuramente
 });
